d764b6122b4149a0dc986209496f40ba1d2dacbc
Claudia Lisboa <ccalumby@gmail.com>
2023-05-14 22:46:43 -0400

    install material-symbols

--- src/Button.jsx
+++ src/Button.jsx
@@ -16,14 +16,22 @@ function Button() {
     const [ isOpen, setIsOpen ] = useState(false);
 
     return (
-        <div className={`fa ${isOpen ? 'open' : ""}`}>
-            <button onClick={() => setIsOpen(! isOpen)}>
+        <div className={`fab ${isOpen ? 'open' : ""}`}>
+            <button onClick={() => setIsOpen(!isOpen)}>
                 <Icon> add </Icon>
             </button>
             <div className='menu'>
                 <button>
                     <Icon> person_add </Icon>
                     <span> Add Users </span>
+                </button>
+                <button>
+                    <Icon>description</Icon>
+                <span>Documents</span>
+                </button>
+                <button>
+                     <Icon>settings</Icon>
+                <span>Settings</span>
                 </button>
                {/* others buttons  */}
             </div>
