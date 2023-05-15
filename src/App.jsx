b5044f3359482f115c8f36d872862bad071c91fc
Claudia Lisboa <ccalumby@gmail.com>
2023-05-14 23:25:25 -0400

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,13 @@ function App() {
   return (
     <>
       <Button/>
+      
+    <footer className="footer">
+          <h4><span className="created-by">Created by Claudia Lisboa</span><span class="studying"> - For studying purpose</span></h4> 
+          <div className='copyright'>© 2023 Copyright :
+          <a href="https://github.com/claudiaLisboa"> https://github.com/claudiaLisboa </a>
+          </div>
+    </footer>
     </>
   )
 }
